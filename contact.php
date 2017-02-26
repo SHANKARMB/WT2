72ae4b11a3c3b32a4b81877d65acb5919a38f5b3
varunlpai <varunlpai.study@gmail.com>
2017-02-26 14:21:38 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Us</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- FlexSlider -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<script defer src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+						$(window).load(function(){
+						  $('.flexslider').flexslider({
+							animation: "slide",
+							start: function(slider){
+							  $('body').removeClass('loading');
+							}
+						  });
+						});
+					  </script>
+<!-- //FlexSlider -->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+	
+<body>
+<!-- banner -->
+	<div class="banner1">
+		<div class="container">
+			<div class="header-nav">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo">
+							<a class="navbar-brand" href="index.php">Project Hub<br><br><span>Collaborate to Innovate!!</span></a>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					  <ul class="nav navbar-nav">
+						<li><a class="btn" href="index.php">Home</a></li>
+						<li><a class="btn" href="repo.php">Code It!</a></li>
+						<li class="active"><a class="btn" href="contact.php">Contact Us</a></li>
+					  </ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</div>
+	</div>
+<!-- //banner -->
+<!-- contact -->
+	<div class="contact">
+		<div class="container">
+			<h3>Contact Us</h3>
+			<div class="line">
+			</div>
+			<p class="proident">
+				Preetham M G<br/>
+				Shankar M B<br/>
+				Varun L Pai<br/>
+			</p>
+		</div>
+	</div>
+<!-- //contact -->
+<!-- //banner -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-row">
+				<div class="col-md-3 footer-grids">
+					<h1><a href="index.php">Project Hub</a></h1>
+					
+					<p><a href="mailto:support@BookKart.com">help@ProjectHub.com</a></p>
+					<p>+9180 9847 4321</p>
+					<div class="col-md-6 banner-info-left">
+				
+					<ul>
+						<li><a href="#" class="twitter"></a></li>
+						<li><a href="#" class="g"></a></li>
+						<li><a href="#" class="v"></a></li>
+						<li><a href="#" class="dribble"></a></li>
+					</ul>
+				</div>
+				</div>
+				<div class="col-md-3 footer-grids">
+					<h3>Navigation</h3>					
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 footer-grids">	
+					<h3>Newsletter</h3>
+					<p>It was popularised in the 1960s <p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">		
+			<p>Copyright © 2017 Web Technologies . All rights reserved | Design by <a href="index.php">Project Hub</a></p>					
+		</div>
+	</div>
+<!--//footer-->	
+<!-- for bootstrap working -->
+		<script src="js/bootstrap.js"> </script>
+<!-- //for bootstrap working -->
+</body>
+</html>
