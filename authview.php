39363a8dbba0486fea7feec5ca265d299174e4f7
SHANKARMB <smbinju@gmail.com>
2017-04-09 21:41:48 +0530

    Update authview.php

--- authview.php
+++ authview.php
@@ -10,6 +10,8 @@
 	<!--JavaScript-->
 	<script>
 		function load(){
+			var vars=getUrlVars();
+			$("#iframeChat").attr("src","http://localhost:3000/"+vars["repo"]+"/"+sessionStorage["user"]);
 			$(".username").html(sessionStorage["user"]);
 			fillTable();
 			window.setInterval(function(){
@@ -123,6 +125,8 @@
 			</div>
 		</center>
 	</div>
+		<iframe id="iframeChat" name="iframeChat" style="width:500px;height:500px;float:right" ></iframe>
+
 	
 </body>
 </html>
